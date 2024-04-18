393aa990fe94832dd626944042428ef6b9554076
Fabrício <fabriciotonettolondero@gmail.com>
2024-04-18 13:00:43 -0300

    first commit

---
+++ Scripts/BannerAdScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class BannerAdScript : MonoBehaviour
+{
+	//código do jogo no Android ou IOS
+    string gameId = "xxxx";
+	//código da propaganda
+    string myPlacementId = "Banner_Android";
+	//se a propaganda esta em teste ou não
+    bool testMode = true;
+
+    void Start()
+    {
+		//Inicia a propaganda
+        Advertisement.Initialize(gameId, testMode);
+        //definir local do banner TOP_LEFT, TOP_CENTER, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT, CENTER
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        StartCoroutine(ShowBannerWhenInitialized());
+    }
+
+    IEnumerator ShowBannerWhenInitialized()
+    {
+        while (!Advertisement.isInitialized)
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.Show(myPlacementId);
+    }
+}
---
+++ Scripts/InterstitialAdsScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InterstitialAdsScript : MonoBehaviour
+{
+    string gameId = "xxxx";
+	string myPlacementId = "Interstitial_Android";
+    bool testMode = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    public void ShowInterstitialAd()
+    {
+        // Check if UnityAds ready before calling Show method:
+        //if (Advertisement.IsReady())
+        //{
+        //    Advertisement.Show(myPlacementId);
+        Advertisement.Show(myPlacementId);
+        //}
+        //else
+        //{
+        //    Debug.Log("Interstitial ad not ready at the moment! Please try again later!");
+        //}
+    }
+}
